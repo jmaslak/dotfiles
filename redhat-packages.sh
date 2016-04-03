8894e09fcd38336bce63ef6dd1f433cedbe1b396
Joel Maslak <jmaslak@antelope.net>
2016-04-03 08:28:58 -0600

    Start of scaffolding to install appropriate Redhat RPMs

---
+++ redhat-packages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2016 Joel C. Maslak
+# All Rights Reserved - See License
+#
+
+doit() {
+    yum -y groupinstall 'Development Tools'
+    yum -y install libcap-devel
+    yum -y install libX11-devel
+    yum -y install mysql-devel
+    yum -y install openssl-devel
+    yum -y install postgresql-devel
+    yum -y install readline-devel
+
+    ### REDHAT 5:
+    wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-5.noarch.rpm
+    rpm --install epel-release-latest-5.noarch.rpm
+    rm epel-release-latest-5.noarch.rpm
+    yum update
+    yum -y install git
+}
+
+doit
+
+
