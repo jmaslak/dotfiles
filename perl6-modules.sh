44df29993236d9c1594d63e84650e8edba5d66ba
Joelle Maslak <jmaslak@antelope.net>
2018-06-20 16:25:17 -0600

    Update to more modern Perl 6 support (with zef)

--- perl6-modules.sh
+++ perl6-modules.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #
-# Copyright (C) 2016 Joelle Maslak
+# Copyright (C) 2016-2018 Joelle Maslak
 # All Rights Reserved - See License
 #
 
 doit() {
-    which panda >/dev/null
+    which zef >/dev/null
     if [ $? -eq 0 ] ; then
         install_modules
     else
         echo ""
         echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-        echo "You should install Rakudo Star (Perl 6)."
+        echo "You should install Perl 6 and zef."
         echo ""
         echo "Not installing Perl 6 modules."
         echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
@@ -21,14 +21,15 @@ doit() {
 }
 
 install_modules() {
+    install_module App::Mi6
     install_module StrictNamedArguments
 }
 
 install_module() {
     MODULE="$1"
-    panda --installed list | egrep "^$MODULE " >/dev/null
+    zef locate "$MODULE" 2>/dev/null >/dev/null
     if [ $? -ne 0 ] ; then
-        panda install $MODULE
+        zef install "$MODULE"
     else
         echo "$MODULE already installed."
     fi
