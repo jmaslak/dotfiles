f5ae74c9e85a98590e0db22535fc3ffc374fdf17
Joelle Maslak <jmaslak@antelope.net>
2019-01-23 22:26:04 -0700

    Set up PATH correctly

--- python-install.sh
+++ python-install.sh
@@ -38,7 +38,7 @@ doit() {
         cd "$HOME"
         git clone git://github.com/yyuu/pyenv.git .pyenv
         export PYENV_ROOT="$HOME/.pyenv"
-        export PATH="$PYENV_ROOT:$PATH"
+        export PATH="$PYENV_ROOT/bin:$PATH"
         eval "$(pyenv init -)"
     fi
 
