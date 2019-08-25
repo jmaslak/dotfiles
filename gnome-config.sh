d557eb55e76191667f62479ac285b0744f2fc14a
Joelle Maslak <jmaslak@antelope.net>
2019-08-25 11:22:42 -0600

    Gnome configuration

---
+++ gnome-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2019 Joelle Maslak
+# All Rights Reserved - See License
+#
+
+# Disable auto-resizing of windows
+gsettings set org.gnome.mutter edge-tiling false
+
