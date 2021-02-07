8c196ef1d5309191b339ab3f6ad62ee5c0a865e6
Joelle Maslak <jmaslak@antelope.net>
2021-02-07 16:42:08 -0700

    Update Python versions

--- python-install.sh
+++ python-install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 #
-# Copyright (C) 2020 Joelle Maslak
+# Copyright (C) 2020-2021 Joelle Maslak
 # All Rights Reserved - See License
 #
 
-PYTHON2=2.7.18
-PYTHON3=3.8.5
+PYTHON36=3.6.12
+PYTHON37=3.7.9
+PYTHON38=3.8.7
+PYTHON39=3.9.0
 
 doit() {
     # Defensive umask
@@ -31,19 +33,23 @@ doit() {
             export PATH="$PYENV_ROOT/bin:$PATH"
             eval "$(pyenv init -)"
         fi
+
+        cd "$HOME/.pyenv/" && git pull && cd -
     fi
 
     # Install pyenv-virtualenv
     if [ ! -d "$HOME/.pyenv/plugins/pyenv-virtualenv" ] ; then
         cd "$HOME/.pyenv/plugins"
         git clone git://github.com/yyuu/pyenv-virtualenv.git pyenv-virtualenv
-    fi    
+    fi
 
-    install $PYTHON2
-    install $PYTHON3
+    install $PYTHON36
+    install $PYTHON37
+    install $PYTHON38
+    install $PYTHON39
 
-    echo "Setting Python version to $PYTHON3"    
-    pyenv global "$PYTHON3"
+    echo "Setting Python version to $PYTHON39"
+    pyenv global "$PYTHON39"
     pyenv rehash
 
     cd "$CWD"
