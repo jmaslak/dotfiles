881b734b3b99be7b6e1f2602d95efe499cbaa016
Joelle Maslak <jmaslak@antelope.net>
2024-05-24 11:06:09 -0600

    Stop redis before starting it

--- src/bin/start-redis.sh
+++ src/bin/start-redis.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 #
-# Copyright (C) 2023 Netflix
+# Copyright (C) 2023-2024 Netflix
 # All Rights Reserved - See License
 #
 
 doit() {
+    ID=$(docker ps -f name=redis-stack-server --all --format '{{.ID}}')
+    if [ "$ID" != "" ] ; then
+        docker rm "$ID"
+    fi
     docker run -d --name redis-stack-server -p 6379:6379 redis/redis-stack-server:latest
 }
 
