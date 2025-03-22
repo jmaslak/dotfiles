95504e06a1736c94ae9a40e091cd56f4b1c0702c
Joelle Maslak <jmaslak@antelope.net>
2025-03-21 23:12:58 -0600

    Start of Mac setup

---
+++ mac-packages.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2025 Joelle Maslak
+# All Rights Reserved - See License
+#
+
+# Install some packages needed for other tasks (such as building Perl)
+
+doit() {
+    brew install tmux
+}
+
+foo() {
+    sudo apt-get update
+    # entr = Execute things when a file is modified
+    sudo apt-get install -y \
+        bash-completion \
+        bc \
+        build-essential \
+        cmake \
+        curl \
+        dnsutils \
+        entr \
+        ethtool \
+        expect \
+        fonts-noto \
+        git \
+        graphicsmagick \
+        imagemagick \
+        iputils-ping \
+        libbz2-dev \
+        libc-ares-dev \
+        libffi-dev \
+        libglib2.0-dev \
+        liblzma-dev \
+        libncurses-dev \
+        libpodofo-utils \
+        libpq-dev \
+        libreadline-dev \
+        libsqlite3-dev \
+        libssl-dev \
+        liblzma-dev \
+        lzma \
+        man \
+        mupdf-tools \
+        ncal \
+        netcat \
+        net-tools \
+        postgresql-common \
+        rsync \
+        shellcheck \
+        sqlite3 \
+        tmux \
+        traceroute \
+        unzip \
+        vim \
+        virtualenv \
+        wget \
+        whois \
+        zlib1g-dev
+    sudo apt-get install -y \
+        neovim
+    sudo apt-get install -y docker.io
+    sudo apt-get install -y ubuntu-release-upgrader-core
+    sudo apt-get install -y libfindbin-libs-perl
+    sudo apt-get install -y libusb-1.0-0.dev
+    sudo apt-get install -y time
+    sudo apt-get install -y python3-autopep8
+    sudo apt-get install -y python3-autopep8
+    sudo apt-get install -y python3-venv tox
+    sudo apt-get install -y rustc
+    sudo apt-get install -y linux-firmware
+
+    if [ "$DISPLAY" != "" ] ; then
+        sudo apt-get install -y emacs-gtk
+    fi
+}
+
+doit "$@"
+
+
