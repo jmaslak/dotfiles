e8e18193de523a7b0160b47308321e7b2eb8e4d6
Joelle Maslak <jmaslak@antelope.net>
2025-11-28 13:09:24 -0700

    Provide default output parameter in format-scan-pdf.sh

--- src/bin/format-scan-pdf.sh
+++ src/bin/format-scan-pdf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-# Copyright (C) 2023-2024 Joelle Maslak
+# Copyright (C) 2023-2025 Joelle Maslak
 # All Rights Reserved - See License
 #
 
@@ -13,6 +13,9 @@ fi
 
 SRC="$1"
 DST="$2"
+if DST="" ; then
+    DST="$1"
+fi
 cp "$SRC" ~/pdf/in.$$.pdf
 docker run -it --user $(id -u):$(id -g) -v ~/pdf:/usr/pdf jmaslak/format-scan-pdf in.$$.pdf out.$$.pdf
 cp ~/pdf/out.$$.pdf "$DST"
