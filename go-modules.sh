4e28d28948006ca7d24445a217f9488285e45155
Joelle Maslak <jmaslak@antelope.net>
2026-09-05 12:37:02 -0600

    Update module list

--- go-modules.sh
+++ go-modules.sh
@@ -10,11 +10,14 @@ doit() {
     go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
     go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
     # go install github.com/go-delve/delve/cmd/dlv@latest
+    go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
+    go install github.com/jmaslak/go-busy-indicator/cmd/...@latest
     go install github.com/rakyll/gotest@latest   # colorized gotest command
     go install github.com/palkan/mulint@latest  # mutex linter
     go install golang.org/x/vuln/cmd/govulncheck@latest  # Go vulnerability checker
     go install github.com/securego/gosec/v2/cmd/gosec@latest  # Go security scanner
     go install github.com/jmaslak/go-router-colorizer/cmd/router-colorizer@latest  # Router colorizer
+    go install honnef.co/go/tools/cmd/staticcheck@latest
 }
 
 doit "$@"
